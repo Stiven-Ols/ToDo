7c7142df1f3eeeaef643d9ecdffa0c93544e81dc
stiven <brayan-steven23@hotmail.com>
2023-06-23 18:17:44 -0500

    clase 02 completada

--- JS/source.js
+++ JS/source.js
@@ -1,5 +1,12 @@
-document.querySelector('.card');
 
-// * textContent permite acceder al contenido de un elemento
-document.querySelector('.task').textContent;
+const btn = document.querySelector('[data-form-btn]');
 
+const createTask = (evento) => {
+    evento.preventDefault();
+    const input = document.querySelector('[data-form-input]');
+    console.log(input.value);
+}
+console.log(btn);
+
+//  * addEventListener escucha eventos de los elementos
+btn.addEventListener('click', createTask );
